fb11e361844bfe1dee287226af7c8f1f8d05f91b
Berstanio <profiplayer2000@gmail.com>
2017-11-24 18:31:59 +0100

    Initial commit

---
+++ src/de/berstanio/bedwars/Main.java
@@ -0,0 +1,11 @@
+package de.berstanio.bedwars;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+
+    @Override
+    public void onEnable() {
+        System.out.println("Bedwars aktiviert!");
+    }
+}
